240d290fe94c3857d74d29b7fdeecd0f1e5749c7
조경진 <cho881020@gmail.com>
2021-08-23 09:53:45 +0900

    방 목록 어댑터 뼈대 작업

---
+++ app/src/main/java/com/neppplus/jickbangcopy_20210820/adapters/RoomAdapter.kt
@@ -0,0 +1,33 @@
+package com.neppplus.jickbangcopy_20210820.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20210820.R
+import com.neppplus.jickbangcopy_20210820.datas.RoomData
+
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
+
+
+}
