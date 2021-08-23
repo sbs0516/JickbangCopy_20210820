d98a7f88af3c1cc0834055af3fa63b2e699c1a34
조경진 <cho881020@gmail.com>
2021-08-23 10:20:07 +0900

    가격 가공하는 기능을 RoomData에 추가  - 어댑터에서는 이를 활용.

--- app/src/main/java/com/neppplus/jickbangcopy_20210820/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210820/adapters/RoomAdapter.kt
@@ -33,6 +33,8 @@ class RoomAdapter(
         val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
         val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
 
+        priceTxt.text = data.getFormattedPrice()
+
         descriptionTxt.text = data.description
 
         return row
--- app/src/main/java/com/neppplus/jickbangcopy_20210820/datas/RoomData.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210820/datas/RoomData.kt
@@ -1,5 +1,8 @@
 package com.neppplus.jickbangcopy_20210820.datas
 
+import java.text.NumberFormat
+import java.util.*
+
 class RoomData(
     val price : Int,
     val address : String,
@@ -7,5 +10,24 @@ class RoomData(
     val description : String ) {
 
 //    추가 기능 (함수) 구현
+
+    fun getFormattedPrice() : String {
+
+        if (this.price >= 10000) {
+            val uk = this.price / 10000
+            val rest = this.price % 10000
+
+            val result = "${uk}억${NumberFormat.getNumberInstance(Locale.KOREA).format(rest)}"
+
+            return result
+        }
+        else {
+            val result  = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
+
+            return result
+        }
+
+    }
+
 
 }
